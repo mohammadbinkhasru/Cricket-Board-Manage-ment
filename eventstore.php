5f4b9cab854da80e7eab9ad608bce1aa9bad492a
Tangim Hossain Akash <tangim.akash@northsouth.edu>
2019-05-02 21:57:28 +0600

    edited by akash

---
+++ eventstore.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<body>
+<head>
+
+
+<form action="eventsave.php" method="post">
+
+
+
+Event ID: <input type="number" name="env_id"> 
+Event Name: <input type="text" name="env_name"><br><br>
+Start Date: <input type="date" name="srt_d">
+End Date: <input type="date" name="e_d">
+
+
+
+<br><br>
+<input type="submit" name="submit">
+
+
+</form>
+
+
+
+</body>
+</head>
+</html>
+
