8227e2509e20764d2098387b2e3eb22d08e2056d
Tangim Hossain Akash <tangim.akash@northsouth.edu>
2019-05-03 20:14:30 +0600

    player & club details by akash

---
+++ clubstore.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+<body>
+<head>
+
+<form action="clubsave.php" method="post">
+
+
+
+
+<h2 style="color:green;" align="center" >Club Information</h2>
+
+Club Name : <input type="text" name="clb_nme"><br><br>
+Date Of Establishment: <input type="date" name="d_o_e">
+
+<h3 align="left">Club Address</h3><br>
+
+City : <input type="text" name="cty"> <br><br>
+Phone Number: <input type="number" name="phn_num">
+
+<h3 align="left">president</h3><br>
+
+Name Of President : <input type="text" name="n_o_p"><br><br>
+
+
+
+
+<br><br>
+<input type="submit" name="submit">
+
+
+</form>
+
+
+
+</body>
+</head>
+</html>
+
---
+++ playerstore.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<body>
+<head>
+<h1 align="center">Player Form</h1>
+
+<form action="playersave.php" method="post">
+
+First  name: <input type="text" name="fname">
+Middle name: <input type="text" name="mdname">
+Last   name: <input type="text" name="ltname"><br><br>
+Father name: <input type="text" name="f_name">
+Motrher name: <input type="text" name="m_name"><br><br>
+Date Of Birth: <input type="date" name="d_o_b">
+Player Id: <input type="number" name="pl_id">
+Club Name: <input type="text" name="cl">
+Contract Id: <input type="number" name="ct_id"><br><br>
+
+<h3>Membership</h3><br>
+
+Membership: <input type="text" name="mshp"><br><br>
+
+ <br>Signature: <input type="text" name="sn">
+ <br><br>
+ 
+
+
+<br><br>
+<input type="submit" name="submit">
+
+
+</form>
+
+
+
+</body>
+</head>
+</html>
